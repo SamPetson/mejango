67ec14cc140467831e3f5d0c9a8eb0a269e9c44c
SamPetson <coolej331@gmail.com>
2025-05-01 23:20:58 -0400

    Create workspace.d.js

---
+++ workspace.d.js
@@ -0,0 +1,10 @@
+const debounce = {
+  solidfile = "solid/solid.js",
+  fchecker_file = "solid/fchecker.js",
+
+  // database endpoint
+  endpoint = "https://github.com/SamPetson/mejango/api/write.txt",
+  encryption = true,
+}
+
+  return debounce;
